3c15c03020822194ea61bf3da0fb692c90803b6c
David Kenny <dave@skynet.ie>
2014-04-08 21:50:44 +0100

    Adding more eclipse ignores.. Add skeleton testcase for FixtureResultService

---
+++ Service/src/test/java/ie/logn/fixtureResult/TestFixtureResultService.java
@@ -0,0 +1,66 @@
+package ie.logn.fixtureResult;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ie.logn.DatabaseTest;
+
+public class TestFixtureResultService extends DatabaseTest {
+
+	@Before
+	public void setUp() throws SQLException, FileNotFoundException {
+		super.setUp();
+	}
+
+	@After
+	public void tearDown() throws SQLException, FileNotFoundException {
+		super.tearDown();
+	}
+
+	@Test
+	public void testFixtureResultServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveFixture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFixture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCompetitionFixtures() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResultForFixture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCompetitionResults() {
+		fail("Not yet implemented");
+	}
+
+}
